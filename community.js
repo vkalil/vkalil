30f20cfc0b2cb57865aaa511648f6ef690c229cb
Visconde007 <victorzkalil@gmail.com>
2020-07-07 23:36:10 -0400

    v1.4.1
    content

--- community.js
+++ community.js
@@ -9,25 +9,25 @@ function hamburgerFunction() {
 function displayInfo (whichOne){
     var displayText;
     if(whichOne===1){
-        displayText = "<h3>Event Planning Lead</h3>In its first year, I joined the Google Developer Student Club as Event Planning Lead I helped set up events, study jams, and led initiatives initiatives to get the word out about this exciting new club. I hope to lead it this coming year! Fingers crossed!";
+        displayText = "<h3>Event Planning Lead</h3>In its first year, I set up events, study jams, and led initiatives to get the word out about this exciting new club";
     }
     if(whichOne===2){
-        displayText = "<h3>Head of Outreach</h3>I have an amazing time with the Society of Hispanic Professional Engineers! I worked with a small team on exec to increase the Latinx engineer admits at WashU--we doubled them this year! I've also increased active participation in events";
+        displayText = "<h3>Head of Outreach</h3>I worked with a small team on exec to increase the Latinx engineer admits at WashU--we doubled them this year! I've also increased active participation in events";
     }
     if(whichOne===3){
-        displayText = "<h3>Scholar</h3>When applying to college, I applied to the prestigious Annika Rodriguez scholarship program at WashU. I was one of the 1% of high school students accepted for my work in high school. We regularly work in the community inside and outside of WashU"
+        displayText = "<h3>Scholar</h3>One of few students accepted Annika Rodriguez scholarship program at WashU. We regularly work with the community inside and outside of WashU"
     }
     if(whichOne===4){
-        displayText = "<h3>National Team Member</h3>Was accepted with my team into this rigourous project where we worked with the YMCA on a national level to answer one question: How can we empower youth through the YMCA? We got a lot of positive feedback and we hope to implement our solution fall 2020!"
+        displayText = "<h3>National Team Member</h3>Worked with the YMCA on a national level to answer one question: How can we empower the youth through the YMCA? Implementing fall 2020!"
     }
     if(whichOne===5){
-        displayText = "<h3>Team Member</h3>During my first year of college, I joined DFA and began working with LaunchCode to try and solve their large droupout rates. My team and I worked with them  in order to improve their TAing system to facilitate more of a bond and create a reentry process"
+        displayText = "<h3>Team Member</h3>Collaborated with LaunchCode to try and solve their large droupout rates. Restructured TAing system and created program reentry process"
     }
     if(whichOne===6){
-        displayText = "<h3>Award Recipient</h3>After Parkland, I was outraged and had enough. I led a city-wide effort to bring the youth and community together and fought for gun control legislation through many different events. Received this award for my impact in the community"
+        displayText = "<h3>Award Recipient</h3>Led a city-wide effort to bring the youth and community together and fought for gun control legislation through many different events. Received this award for my impact in the community"
     }
     if(whichOne===7){
-        displayText = "<h3>Executive Editor</h3>I joined The Little Hawk as a reporter and eventually became Executive Editor where I taught classes and bettered the newspaper community at my school. Led our team to get first place news team in Iowa"
+        displayText = "<h3>Executive Editor</h3>Taught classes and bettered the newspaper community at my school. Led our team to get first place news team in Iowa"
 
     }
     console.log(displayText);
--- projects.js
+++ projects.js
@@ -9,19 +9,19 @@ function hamburgerFunction() {
 function displayInfo (whichOne){
     var displayText;
     if(whichOne===1){
-        displayText = "<h3>Summer Intern</h3>Figured out how to break website on accident, so I spent the majority of my time fixing it and implementing website testing frameworks. Worked on other projects as well <h5>HTML, JS, CSS, Python, InfluxDB, Mocha+Chai</h5>"
+        displayText = "<h3>Summer Intern</h3>Brokee website on accident, so I spent the majority of my time fixing it and implementing website testing frameworks. Worked on other projects as well <h5>HTML, JS, CSS, Python, InfluxDB, Mocha+Chai</h5>"
     }
     if(whichOne===2){
-        displayText = "<h3>Final Project</h3>I worked on this project with a partner to create a website that used the Trefle.io API that allowed the user to intuitively query data, take a fun quiz, and learn what kind of plant may be best to grow based on the weather in your area using a weather API <h5>HTML, JS, CSS, APIs</h5>"
+        displayText = "<h3>Final Project</h3>Created website with a partner that interacts with Trefle.io's API and a weather API to do a variety of different tasks <h5>HTML, JS, CSS, APIs</h5>"
     }
     if(whichOne===3){
-        displayText = "<h3>Human-Computer Interaction</h3> Message me for my portfolio! I have designed several apps and websites in and out of courses using several softwares listed below. Creating inclusive and intuitive applications & websites is EXTREMELY important to me <h5>Adobe Xd, Figma, Sketch</h5>"
+        displayText = "<h3>Human-Computer Interaction</h3> Message me for design portfolio! Creating inclusive and intuitive projects and systems is extremely important to me<h5>Adobe Xd, Figma, Sketch</h5>"
     }
     if(whichOne===4){
-        displayText = "<h3>Teaching Assistant</h3>Since sophomore year began, I have been TAing WashU's introductory Java computer science course through studios, labs, and office hours<h5>Java</h5>"
+        displayText = "<h3>Teaching Assistant</h3>TAing WashU's introductory Java computer science course through studios, labs, and office hours<h5>Java</h5>"
     }
     if(whichOne===5){
-        displayText = "<h3>Researcher</h3>In high school, I worked in a lab during my senior year to create a program that could simulate responses to a device designed to assist vision impaired user<h5>Java</h5>"
+        displayText = "<h3>Researcher</h3>In high school, I worked in a lab to simulate responses to a new device that assists near-blind users<h5>Java</h5>"
     }
     if(whichOne===6){
         displayText = "<h3>Interests</h3>I'm highly interested in coding equitable apps, websites, and softwares! I'm comfortable with Java, JS, HTML/CSS, and Swift. I also have experience with C++. On the side, I love rap, sneaker culture, and the NBA"
